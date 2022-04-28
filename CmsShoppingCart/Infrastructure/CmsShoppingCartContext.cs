ccb27c5d4c6f1461a279ce35beed0cd1f18f947c
sphakamiso-git <sparngobese@gmail.com>
2022-04-28 19:47:07 +0200

    Db

---
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext:DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options):base(options)
+        {
+
+        }
+    }
+}
